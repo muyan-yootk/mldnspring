edb5f3333cb4261ca6633fbaeb21ce1ed70329a9
mldn <mldnqa@163.com>
2018-05-28 11:29:28 +0800

    讲解了Spring-Test测试环境的使用，并且利用了@Resource注解实现了代码资源的注入管理，同时分析了Resource注解无法使用的问题，导入了annotations-api的依赖库解决。

--- mldnspring-base/src/test/java/cn/mldn/mldnspring/test/TestMessage.java
+++ mldnspring-base/src/test/java/cn/mldn/mldnspring/test/TestMessage.java
@@ -1,17 +1,25 @@
 package cn.mldn.mldnspring.test;
 
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
+import javax.annotation.Resource;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import cn.mldn.mldnspring.service.IMessage;
-
+// 由于Spring所有的Bean都受到容器管理，则在进行测试的时候就必须明确的设置Spring配置文件的路径
+@ContextConfiguration(locations= {"classpath:spring/spring-base.xml"})
+// 本次将采用Junit进行代码的测试，所以选择测试的运行操作为“SpringJUnit4ClassRunner”
+@RunWith(SpringJUnit4ClassRunner.class)
 public class TestMessage {
-	public static void main(String[] args) {
-		// 利用ApplicationContext同时指定有一个配置文件的加载路径，通过此配置文件启动Spring容器
-		ApplicationContext context = new ClassPathXmlApplicationContext("spring/spring-base.xml");
-		// 容器启动之后会自动进行类的实例化，就可以利用ApplicationContext获取实例化对象
-		IMessage msg = context.getBean("messageImpl",IMessage.class) ;
-		System.out.println(msg.echo("www.mldn.cn"));
-				
+	// @Resource		// 该注解为JavaEE标准注解，提供的是资源注入处理
+	@Autowired 
+	private IMessage message ; // 接口对象
+	@Test
+	public void testEcho() {
+		String echoMsg = this.message.echo("www.mldn.cn") ;
+		System.out.println(echoMsg);
 	}
 }
