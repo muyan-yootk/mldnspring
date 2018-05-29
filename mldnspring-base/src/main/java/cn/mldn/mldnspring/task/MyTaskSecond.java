22adb17f9704e7c952ca0c0c46494170103f8e08
mldn <mldnqa@163.com>
2018-05-29 15:34:32 +0800

    使用线程池处理多个定时任务。

--- mldnspring-base/src/main/java/cn/mldn/mldnspring/task/MyTaskSecond.java
+++ mldnspring-base/src/main/java/cn/mldn/mldnspring/task/MyTaskSecond.java
@@ -2,14 +2,20 @@ package cn.mldn.mldnspring.task;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.concurrent.TimeUnit;
 
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 @Component	// 所有的类一定要在Spring进行注册配置
-public class MyTask {
+public class MyTaskSecond {
 	@Scheduled(cron="* * * * * ?")
 	public void runTask() {  
-		System.out.println("【First - 当前的日期时间】" + new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS").format(new Date()));
+		try {
+			TimeUnit.SECONDS.sleep(10);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		System.out.println("【Second - 当前的日期时间】" + new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS").format(new Date()));
 	}
 
 }
