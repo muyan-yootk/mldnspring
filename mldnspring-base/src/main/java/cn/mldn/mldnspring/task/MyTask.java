3def788fb60cb07a95916abcc2f3ef25e9018700
mldn <mldnqa@163.com>
2018-05-29 14:50:07 +0800

    使用自定义任务方法，实现CRON调度。

--- mldnspring-base/src/main/java/cn/mldn/mldnspring/task/MyTask.java
+++ mldnspring-base/src/main/java/cn/mldn/mldnspring/task/MyTask.java
@@ -3,14 +3,9 @@ package cn.mldn.mldnspring.task;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
-import org.quartz.JobExecutionContext;
-import org.quartz.JobExecutionException;
-import org.springframework.scheduling.quartz.QuartzJobBean;
+public class MyTask {
 
-public class MyTask extends QuartzJobBean {
-
-	@Override
-	protected void executeInternal(JobExecutionContext context) throws JobExecutionException {
+	public void runTask() {  
 		System.out.println("【当前的日期时间】" + new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS").format(new Date()));
 	}
 
