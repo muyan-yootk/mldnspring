f44738d5a64b9227d111221f13ebd80c59fcfda8
mldn <mldnqa@163.com>
2018-05-29 14:40:12 +0800

    使用CRON实现QuartZ调用。

--- mldnspring-base/src/main/java/cn/mldn/mldnspring/main/TimerDemo.java
+++ mldnspring-base/src/main/java/cn/mldn/mldnspring/main/TimerDemo.java
@@ -2,6 +2,7 @@ package cn.mldn.mldnspring.main;
 
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.scheduling.quartz.CronTriggerFactoryBean;
 public class TimerDemo {
 	public static void main(String[] args) {
 		ApplicationContext context = new ClassPathXmlApplicationContext("spring/spring-base.xml") ;
