a0a6cc197de1710eecb6428baab1ea5978724951
mldn <mldnqa@163.com>
2018-05-29 17:41:40 +0800

    实现ActiveMQ的消息发送者

---
+++ mldnspring-activemq-send/src/main/java/cn/mldn/mldnspring/service/IMessageService.java
@@ -0,0 +1,5 @@
+package cn.mldn.mldnspring.service;
+
+public interface IMessageService {
+	public void send(String msg) ;
+}
---
+++ mldnspring-activemq-send/src/test/java/cn/mldn/mldnspring/test/TestMessageService.java
@@ -0,0 +1,20 @@
+package cn.mldn.mldnspring.test;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import cn.mldn.mldnspring.service.IMessageService;
+
+@ContextConfiguration(locations= {"classpath:spring/spring-base.xml"})
+@RunWith(SpringJUnit4ClassRunner.class)
+public class TestMessageService {
+	@Autowired
+	private IMessageService messageService ;
+	@Test 
+	public void testSend() {
+		this.messageService.send("www.mldn.cn");
+	}
+}
