7097defc207aebf993dbfbc4d67a5b7da250244f
mldn <mldnqa@163.com>
2018-05-29 15:23:56 +0800

    SpringTask基于Annotation扫描实现。

--- mldnspring-base/src/main/java/cn/mldn/mldnspring/task/MyTask.java
+++ mldnspring-base/src/main/java/cn/mldn/mldnspring/task/MyTask.java
@@ -3,8 +3,11 @@ package cn.mldn.mldnspring.task;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+@Component	// 所有的类一定要在Spring进行注册配置
 public class MyTask {
-
+	@Scheduled(cron="* * * * * ?")
 	public void runTask() {  
 		System.out.println("【当前的日期时间】" + new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS").format(new Date()));
 	}
