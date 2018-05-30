5fe30f303f4a456c2ce38a433e58a9c6784da7f7
mldn <mldnqa@163.com>
2018-05-30 10:00:19 +0800

    实现前置通知的参数配置。

--- mldnspring-base/src/main/java/cn/mldn/mldnspring/aop/service/ServiceAdvice.java
+++ mldnspring-base/src/main/java/cn/mldn/mldnspring/aop/service/ServiceAdvice.java
@@ -1,8 +1,8 @@
 package cn.mldn.mldnspring.aop.service;
 
 public class ServiceAdvice {
-	public void handleBefore() {
-		System.err.println("【ServiceAdvice-Before】业务调用执行之前。");
+	public void handleBefore(Object arg) {
+		System.err.println("【ServiceAdvice-Before】业务调用执行之前。" + arg);
 	}
 	public void handleAfter() {
 		System.err.println("【ServiceAdvice-After】业务调用执行之后。");
