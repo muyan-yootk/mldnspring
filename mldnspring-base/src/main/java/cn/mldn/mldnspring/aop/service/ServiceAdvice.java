c71de444a205d75015656df1f67532b13f45d94e
mldn <mldnqa@163.com>
2018-05-30 10:18:23 +0800

    配置AOP环绕通知。

--- mldnspring-base/src/main/java/cn/mldn/mldnspring/aop/service/ServiceAdvice.java
+++ mldnspring-base/src/main/java/cn/mldn/mldnspring/aop/service/ServiceAdvice.java
@@ -1,6 +1,24 @@
 package cn.mldn.mldnspring.aop.service;
 
+import java.util.Arrays;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+
 public class ServiceAdvice {
+	public Object handleAround(ProceedingJoinPoint point) throws Throwable {
+		System.out.println("1、***** 〖环绕通知-BEFORE〗方法调用之前，参数：" + Arrays.toString(point.getArgs()));
+		Object result = null ; // 保存返回结果
+		try {
+			result = point.proceed(point.getArgs()) ;// 调用真实业务主题
+		} catch (Exception e) {
+			System.out.println("2、***** 〖环绕通知-THROW〗调用产生异常：" + e);
+			throw e ; // 将异常抛给调用处
+		}
+		System.out.println("3、***** 〖环绕通知-AFTER〗调用完毕");
+		System.out.println("4、***** 〖环绕通知-RETURN〗返回调用结果：" + result); 
+		return result ;
+	}
+	
 	public void handleBefore(Object arg) {
 		System.err.println("【ServiceAdvice-Before】业务调用执行之前。" + arg);
 	}
