7a98d72968f331f6383971fcc8e04a45568cae73
mldn <mldnqa@163.com>
2018-05-30 10:25:18 +0800

    基于Annotation讲解AOP

--- mldnspring-base/src/main/java/cn/mldn/mldnspring/aop/service/ServiceAdvice.java
+++ mldnspring-base/src/main/java/cn/mldn/mldnspring/aop/service/ServiceAdvice.java
@@ -3,8 +3,17 @@ package cn.mldn.mldnspring.aop.service;
 import java.util.Arrays;
 
 import org.aspectj.lang.ProceedingJoinPoint;
-
+import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.AfterReturning;
+import org.aspectj.lang.annotation.AfterThrowing;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.springframework.stereotype.Component;
+@Component
+@Aspect	// 表示现在要进行的是一个AOP的配置处理类
 public class ServiceAdvice {
+	@Around("execution(public * cn.mldn..service..*.*(..))")
 	public Object handleAround(ProceedingJoinPoint point) throws Throwable {
 		System.out.println("1、***** 〖环绕通知-BEFORE〗方法调用之前，参数：" + Arrays.toString(point.getArgs()));
 		Object result = null ; // 保存返回结果
@@ -18,16 +27,19 @@ public class ServiceAdvice {
 		System.out.println("4、***** 〖环绕通知-RETURN〗返回调用结果：" + result); 
 		return result ;
 	}
-	
+	@Before(value="execution(public * cn.mldn..service..*.*(..)) and args(vo)",argNames="vo")
 	public void handleBefore(Object arg) {
 		System.err.println("【ServiceAdvice-Before】业务调用执行之前。" + arg);
 	}
+	@AfterReturning(value="execution(public * cn.mldn..service..*.*(..))",returning="val",argNames="val")
 	public void handleReturn(Object result) {	// 后置返回通知，方法执行完毕返回结果
 		System.err.println("【ServiceAdvice-Return】方法返回，返回结果：" + result);
 	}
+	@AfterThrowing(value="execution(public * cn.mldn..service..*.*(..))",throwing="e",argNames="e")
 	public void handleThrow(Exception exp) {
 		System.err.println("【ServiceAdvice-Throw】方法执行抛出异常：" + exp);
 	} 
+	@After("execution(public * cn.mldn..service..*.*(..))")
 	public void handleAfter() {	// 后置最终通知
 		System.err.println("【ServiceAdvice-After】业务调用执行之后。");
 	}
