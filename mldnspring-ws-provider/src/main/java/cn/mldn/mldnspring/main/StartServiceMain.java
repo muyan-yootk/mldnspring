3037b0b9293ee06f06b8e836bc4114e2489d1a3b
mldn <mldnqa@163.com>
2018-05-30 11:49:46 +0800

    使用Spring整合WebService

--- mldnspring-ws-provider/src/main/java/cn/mldn/mldnspring/main/StartServiceMain.java
+++ mldnspring-ws-provider/src/main/java/cn/mldn/mldnspring/main/StartServiceMain.java
@@ -3,12 +3,13 @@ package cn.mldn.mldnspring.main;
 
 import javax.xml.ws.Endpoint;
 
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 import cn.mldn.mldnspring.service.IMessageService;
 import cn.mldn.mldnspring.service.impl.MessageServiceImpl;
 
 public class StartServiceMain {
 	public static void main(String[] args) {
-		IMessageService service = new MessageServiceImpl();
-		Endpoint.publish("http://192.168.28.87:7777/message", service) ;
+		new ClassPathXmlApplicationContext("spring/spring-*.xml") ;
 	}
-}
+} 
