6d7837c20cb994b6567a0a35c35f1623ac458ea3
mldn <mldnqa@163.com>
2018-05-31 14:41:09 +0800

    实现了一个基础的Echo程序模型。并且配置了日志组件。

--- mldnspring-mvc/src/main/java/cn/mldn/mldnspring/action/EchoAction.java
+++ mldnspring-mvc/src/main/java/cn/mldn/mldnspring/action/EchoAction.java
@@ -1,8 +1,26 @@
 package cn.mldn.mldnspring.action;
 
-import org.springframework.web.context.ContextLoaderListener;
-import org.springframework.web.filter.CharacterEncodingFilter;
-import org.springframework.web.servlet.DispatcherServlet;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
+@Controller
+@RequestMapping("/pages/message/*")	
 public class EchoAction {
+	@RequestMapping(value="input",method=RequestMethod.GET) 
+	public String input() {
+		return "/pages/message/input.jsp" ; 
+	}
+	/**
+	 * 进行用户的Echo处理逻辑，在请求参数前追加有“ECHO”的信息
+	 * @param msg 用户发送的数据
+	 * @return 处理后的ECHO内容
+	 */
+	@RequestMapping(value="echo",method=RequestMethod.POST) 
+	public ModelAndView echo(String msg) {
+		ModelAndView mav = new ModelAndView("/pages/message/show.jsp") ;
+		mav.addObject("echoMessage", "【ECHO】" + msg) ; // 设置request属性
+		return mav ;
+	}
 }
