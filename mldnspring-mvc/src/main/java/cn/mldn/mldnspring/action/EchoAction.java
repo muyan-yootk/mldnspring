ad05da6572f775c9ba3c0a6d888d014d223f4ea3
mldn <mldnqa@163.com>
2018-05-31 14:19:28 +0800

    搭建SpringMVC开发环境。

---
+++ mldnspring-mvc/src/main/java/cn/mldn/mldnspring/action/EchoAction.java
@@ -0,0 +1,8 @@
+package cn.mldn.mldnspring.action;
+
+import org.springframework.web.context.ContextLoaderListener;
+import org.springframework.web.filter.CharacterEncodingFilter;
+import org.springframework.web.servlet.DispatcherServlet;
+
+public class EchoAction {
+}
