a9acc41b91927fcd5cf88a3b5e1640b396dad78b
mldn <mldnqa@163.com>
2018-05-31 13:38:09 +0800

    声明式事务处理。

--- mldnspring-base/src/main/java/cn/mldn/mldnspring/service/INewsService.java
+++ mldnspring-base/src/main/java/cn/mldn/mldnspring/service/INewsService.java
@@ -5,7 +5,6 @@ import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
 public interface INewsService {
-//	@Transactional(propagation=Propagation.REQUIRED,isolation=Isolation.DEFAULT,readOnly=true)
-	@Transactional(propagation=Propagation.REQUIRED,isolation=Isolation.DEFAULT)
 	public boolean remove(long nid) ;
 }
+ 
