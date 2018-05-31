f509ebfae4ea66fbe24696c360ee05384635e761
mldn <mldnqa@163.com>
2018-05-31 16:20:07 +0800

    获取内置对象。

--- mldnspring-mvc/src/main/java/cn/mldn/util/action/AbstractAction.java
+++ mldnspring-mvc/src/main/java/cn/mldn/util/action/AbstractAction.java
@@ -2,15 +2,27 @@ package cn.mldn.util.action;
 
 import java.text.SimpleDateFormat;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.beans.propertyeditors.CustomDateEditor;
 import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.InitBinder;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
 
 public abstract class AbstractAction {
 	@InitBinder
 	public void initBinder(WebDataBinder binder) {
 		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd") ;
 		binder.registerCustomEditor(java.util.Date.class, new CustomDateEditor(sdf, true));	
+	}
+	public HttpServletResponse getResponse() { 
+		return ((ServletRequestAttributes) RequestContextHolder.getRequestAttributes()).getResponse() ;
+	} 
+	
+	public HttpServletRequest getRequest() {
+		return ((ServletRequestAttributes) RequestContextHolder.getRequestAttributes()).getRequest() ;
 	}
 }
  
