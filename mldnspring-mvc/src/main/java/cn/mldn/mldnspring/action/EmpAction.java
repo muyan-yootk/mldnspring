a2de667b5850faac434474a78883edbfd2b405a7
mldn <mldnqa@163.com>
2018-06-01 09:51:25 +0800

    HanlderMethod类作用。

--- mldnspring-mvc/src/main/java/cn/mldn/mldnspring/action/EmpAction.java
+++ mldnspring-mvc/src/main/java/cn/mldn/mldnspring/action/EmpAction.java
@@ -5,6 +5,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
--- mldnspring-mvc/src/main/java/cn/mldn/util/interceptor/ValidationInterceptor.java
+++ mldnspring-mvc/src/main/java/cn/mldn/util/interceptor/ValidationInterceptor.java
@@ -5,26 +5,20 @@ import javax.servlet.http.HttpServletResponse;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.web.method.HandlerMethod;
 import org.springframework.web.servlet.HandlerInterceptor;
-import org.springframework.web.servlet.ModelAndView;
 
 public class ValidationInterceptor implements HandlerInterceptor {
 	private Logger logger = LoggerFactory.getLogger(ValidationInterceptor.class) ;	// 日志对象
 	@Override
 	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
 			throws Exception {
-		this.logger.info("1、preHandle()方法执行，" + handler.getClass());
-		return false ;	// 如果返回true则表示继续向后执行后续的Action，如果是false表示不执行Action
+		if (handler instanceof HandlerMethod) {	// 判断对象是否为指定类的实例
+			HandlerMethod handlerMethod = (HandlerMethod) handler ; // 对象向下转型
+			this.logger.info("Action对象：" + handlerMethod.getBean());
+			this.logger.info("Action类：" + handlerMethod.getBeanType());
+			this.logger.info("Action方法：" + handlerMethod.getMethod());
+		} 
+		return true ;	// 如果返回true则表示继续向后执行后续的Action，如果是false表示不执行Action
 	}
-	@Override
-	public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler,
-			ModelAndView modelAndView) throws Exception {
-		this.logger.info("2、postHandler()方法执行，handler = " + handler.getClass() + "、ModelAndView = " + modelAndView);
-	}
-	@Override
-	public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex)
-			throws Exception { 
-		this.logger.info("3、afterCompletion()方法执行、" + handler.getClass());
-	}
-	
 }
